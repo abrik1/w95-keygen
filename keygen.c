c6ef1998b231a9a76002ab25197755b4ee14305f
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-09-19 06:50:22 +0000

    fix major bugs and generate retail keys..
    - date overflow fixed.. sometimes numbers above 366 was generated
    - length overflow fixed for OEM and Retail keys like in retail the last element would be of 7 digits.. sometimes 8 digit would be generated it was happening in 4th and last element of the oem key

--- keygen.c
+++ keygen.c
@@ -6,10 +6,27 @@
 int year_format[5] = {95, 96, 97, 98, 99};
 int num = 0;
 int sum = 0;
+int date = 0;
+int key_mode = 0; // 0 if OEM.. 1 if retail
+
+int randint(int min, int max){
+  for(;;){
+    int num = rand() % max + min;
+    if(num > max){
+      continue;
+    } else{
+      return num;
+    }
+  }
+}
 
 int gen_n1(){
   for(;;){
-    int num = rand() % 99999 + 9999;
+    if(key_mode == 0){
+      num = randint(9999, 99999);
+    } else{
+      num = randint(999999, 9999999);
+    }
     int num2 = num;
     int sum = 0;
     while(num != 0){
@@ -17,7 +34,18 @@ int gen_n1(){
       num /= 10;
     }
     if(sum % 7 == 0){
-      return num2;
+     return num2; 
+    } 
+  }
+}
+
+int gen_retail_n1(){
+  for(;;){
+    int num = randint(100, 999);
+    if (num != 111 && num % 111 == 0){
+      continue;
+    } else{
+      return num;
     }
   }
 }
@@ -25,6 +53,8 @@ int gen_n1(){
 int main(){
   srand((unsigned int)time(NULL));
   int n = gen_n1();
-  printf("%d%d-OEM-00%d-%d\n", rand() % 366 + 100, year_format[rand() % 4], n, rand() % 99999 + 9999);
+  printf("OEM: %d%d-OEM-00%d-%d\n", randint(100, 366), year_format[rand() % 4], n, randint(10000, 99999));
+  key_mode = 1;
+  printf("Retail: %d-%d\n", gen_retail_n1(), gen_n1());
   return 0;
 }
